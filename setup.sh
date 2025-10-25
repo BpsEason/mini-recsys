98863327a72b74a9383e514591888a8a68853657
ChenLab <k4917552@gmail.com>
2025-10-25 11:16:48 +0800

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "開始 Mini-RecSys 教學流程..."
-python -m src.preprocess.split
-python -m src.train.train_popularity
-echo "啟動 API：http://localhost:8000/docs"
-uvicorn src.serving.app:app --reload
